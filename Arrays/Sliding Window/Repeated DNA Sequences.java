ed007c2b22469755d7ac8c058bd8446a7a1ad91b
Vidisha Chauhan <vidisha1008chauhan@gmail.com>
2026-07-15 22:04:10 +0530

    Sliding Window Update

---
+++ Arrays/Sliding Window/Repeated DNA Sequences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+
+        if(s.length() < 10) return new ArrayList<>();
+
+        Set seen = new HashSet();
+        Set repeated = new HashSet();
+        
+        for (int i = 0; i + 9 < s.length(); i++) {
+            String ten = s.substring(i, i + 10);
+            if (!seen.add(ten)) repeated.add(ten);
+        }
+        return new ArrayList(repeated);
+    }
+}
